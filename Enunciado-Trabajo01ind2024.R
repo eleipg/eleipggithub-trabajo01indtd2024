6e93eae10c5d4d30a8da9480d3217f3bfc7a7b52
eleipg <elenaipg@gmail.com>
2024-10-18 17:27:10 +0200

    Update Enunciado-Trabajo01ind2024.R

--- Enunciado-Trabajo01ind2024.R
+++ Enunciado-Trabajo01ind2024.R
@@ -1,19 +1,18 @@
 
 # Trabajo 01: Teoría de la Decisión bajo incertidumbre.
 
-### Problema 1: ----
-#Aplicar los criterios de decisión bajo incertidumbre al problema cuya
-#matriz de valores numéricos viene dadas en la tabla siguiente:
-tabla1=crea.tablaX(c(5,3,-2,
-                     2,-1,1,
-                     -1,3,1),numalternativas=3,numestados=3)
-tabla1
-### Problema 2: ----
-#Inés busca trabajo en una ciudad nueva, para ello está buscando piso y ha
-#encontrado dos opciones que se ajustan a sus necesidades, una de ellas se
-#encuentra a 5 minutos andando del trabajo y la otra a 1 hora en autobus. Su
-#sueldo si consigue el trabajo sería de 1500€ al mes, si elige vivir cerca no
-#tendrá gastos en transporte ni en comer fuera pero el alquiler supondría el 60%
-#de su sueldo, mientras que si escoge vivir lejos deberá gastar el 5% en
-#transporte, el 20% en comida y el 25% en alquiler. ¿Qué decisión tomará Inés si
-#no sabe si conseguirá el trabajo?
+# Elena Inés Palomino Galiana
+
+### **Problema 1: Tabla de decisión**
+
+|     | d1  | d2  | d3  |
+|-----|-----|-----|-----|
+| e1  | 5   | 3   | -2  |
+| e2  | 2   | -1  | 1   |
+| e3  | -1  | 3   | 1   |
+
+### **Problema 2: La búsqueda de empleo**
+
+Inés busca trabajo en una ciudad nueva, para ello está buscando piso y ha encontrado dos opciones que se ajustan a sus necesidades, una de ellas se encuentra a 5 minutos andando del trabajo y la otra a 1 hora en autobus.
+Su sueldo si consigue el trabajo sería de 1500€ al mes, si elige vivir cerca no tendrá gastos en transporte ni en comer fuera pero el alquiler supondría el 60% de su sueldo, mientras que si escoge vivir lejos deberá gastar el 5% en transporte, el 20% en comida y el 25% en alquiler.
+¿Qué decisión tomará Inés si no sabe si conseguirá el trabajo?
